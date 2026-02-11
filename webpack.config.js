fa55ac10657d35190a0ceb4114d01bb3bf92f909
unknown <thiiaagomedeiros@gmail.com>
2026-02-11 13:34:50 -0300

    Fix Netlify build configuration

--- webpack.config.js
+++ webpack.config.js
@@ -7,13 +7,21 @@ module.exports = {
 	output: {
 		path: path.resolve(__dirname, 'dist'),
 		filename: 'src/bundle.js',
+		publicPath: './'
+    },
+    resolve: {
+        alias: {
+            '/images': path.resolve(__dirname, 'app/images')
+        }
     },
     plugins: [
-        new CopyWebpackPlugin([
-            { from: 'app/images', to: 'images' },
-            { from: 'app/js/lib', to: 'src/lib' },
-            { from: 'app/index.html', to: 'index.html' }
-        ]),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: 'app/images', to: 'images' },
+                { from: 'app/js/lib', to: 'src/lib' },
+                { from: 'app/index.html', to: 'index.html' }
+            ]
+        }),
     ],
     module: {
         rules: [{
@@ -21,7 +29,10 @@ module.exports = {
             use: [{
                 loader: "style-loader"
             }, {
-                loader: "css-loader"
+                loader: "css-loader",
+                options: {
+                    url: false
+                }
             }, {
                 loader: "sass-loader"
             }]
